c9beb404d62628377b99132b5f25ef8423e7ee64
sanamkader <diyasana3966@gmail.com>
2016-09-13 21:46:06 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/programminglanguage/vibgyor/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.programminglanguage.vibgyor;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtViolet,txtIndigo,txtBlue,txtGreen,txtYellow,txtOrange,txtRed;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        txtViolet = (TextView) findViewById(R.id.textviolet);
+//        txtIndigo =(TextView) findViewById(R.id.textindigo);
+//        txtBlue = (TextView) findViewById(R.id.txtblue);
+//        txtGreen = (TextView) findViewById(R.id.txtgreen);
+//        txtYellow = (TextView) findViewById(R.id.txtyellow);
+//        txtOrange = (TextView) findViewById(R.id.txtorange);
+//        txtRed  =(TextView) findViewById(R.id.txtred);
+//
+//
+//        txtViolet.setBackgroundColor(Color.parseColor("#9400D3"));
+
+
+
+
+
+
+    }
+}
